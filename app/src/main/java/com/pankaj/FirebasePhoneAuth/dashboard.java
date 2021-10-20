59084afcdfe031619bb84f543a5644933c946c19
pankaj singh <pankajsingh8540@gmail.com>
2021-10-20 16:16:02 +0530

    final commit

--- app/src/main/java/com/pankaj/FirebasePhoneAuth/dashboard.java
+++ app/src/main/java/com/pankaj/FirebasePhoneAuth/dashboard.java
@@ -1,8 +1,6 @@
-.
-        package com.pankaj.FirebasePhoneAuth;
+package com.pankaj.FirebasePhoneAuth;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
